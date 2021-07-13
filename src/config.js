05c9d05af5e93859466862672dc6cfcc42c748f4
Kanika Mehra <kanmehra@deloitte.com.au>
2021-07-13 23:03:25 +1000

    firebase write and read works

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config={
+    apiKey: "AIzaSyDlAoXYX7sqXq3XBQv8ToMHZ6r-zEah8uM",
+    authDomain: "sample-project-c50f2.firebaseapp.com",
+    databaseURL: "https://sample-project-c50f2-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "sample-project-c50f2",
+    storageBucket: "sample-project-c50f2.appspot.com",
+    messagingSenderId: "114179397271",
+    appId: "1:114179397271:web:8b2fb9bf41fd3619b1fbd2",
+    measurementId: "G-93854L8EYT"
+
+}
+
+export default config
